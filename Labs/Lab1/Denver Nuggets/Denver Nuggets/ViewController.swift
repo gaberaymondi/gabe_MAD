64a3a1e7857cf403ac6d2d06f59004759f194075
Gabe <gara2591@colorado.edu>
2019-09-11 16:41:42 -0600

    Lab 1

---
+++ Labs/Lab1/Denver Nuggets/Denver Nuggets/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Denver Nuggets
+//
+//  Created by Gabe Raymondi on 9/11/19.
+//  Copyright © 2019 Gabe Raymondi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
