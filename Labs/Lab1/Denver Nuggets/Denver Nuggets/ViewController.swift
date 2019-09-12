9513550884c8032560108ae4bf39e14f8730893f
Gabe <gara2591@colorado.edu>
2019-09-11 18:35:49 -0600

    Lab 1

--- Labs/Lab1/Denver Nuggets/Denver Nuggets/ViewController.swift
+++ Labs/Lab1/Denver Nuggets/Denver Nuggets/ViewController.swift
@@ -9,12 +9,45 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var playerName: UILabel!
+    
+    @IBAction func playerJamal(_ sender: UIButton) {
+        playerName.text = "Jamal Murray"
+    }
+    
+    @IBAction func playerGary(_ sender: UIButton) {
+        playerName.text = "Gary Harris"
+    }
 
+    @IBOutlet weak var playerImage: UIImageView!
+    
+    @IBAction func selectPlayer(_ sender: UIButton) {
+        if sender.tag == 1{
+            playerImage.image = UIImage(named: "Jamal")
+        }
+        else if sender.tag == 2{
+            playerImage.image = UIImage(named: "Gary")
+        }
+    }
+    
+    @IBAction func selectMPJ(_ sender: UIButton) {
+        if sender.tag == 3{
+            playerImage.image = UIImage(named: "MPJ")
+        }
+        playerName.text = "Michael Porter Jr."
+    }
+    
+    @IBAction func selectJokic(_ sender: UIButton) {
+        if sender.tag == 5{
+            playerImage.image = UIImage(named: "Nikola")
+        }
+        playerName.text = "Nikola Jokic"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
