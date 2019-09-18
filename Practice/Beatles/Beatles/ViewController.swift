eacc60ab1f7cfb3531b27078871c3a7adb4f086a
Gabe <gara2591@colorado.edu>
2019-09-18 16:23:46 -0600

    Lecutre

--- Practice/Beatles/Beatles/ViewController.swift
+++ Practice/Beatles/Beatles/ViewController.swift
@@ -12,8 +12,11 @@ class ViewController: UIViewController {
     @IBOutlet weak var beatlesImage: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var imageControl: UISegmentedControl!
+    @IBOutlet weak var fontSizeLabel: UILabel!
+    @IBOutlet weak var fontSizeNumber: UILabel!
+    @IBOutlet weak var captialSwitch: UISwitch!
     
-    @IBAction func changeInfo(_ sender: UISegmentedControl) {
+    func updateImage() {
         if imageControl.selectedSegmentIndex == 0 {
             titleLabel.text = "Young Beatles"
             beatlesImage.image = UIImage(named: "Vevo")
@@ -23,6 +26,31 @@ class ViewController: UIViewController {
             beatlesImage.image = UIImage(named: "Oldie")
         }
     }
+    
+    func updateCaps() {
+        if captialSwitch.isOn {
+            titleLabel.text = titleLabel.text?.uppercased()
+        } else {
+            titleLabel.text = titleLabel.text?.lowercased()
+        }
+    }
+    
+    @IBAction func changeInfo(_ sender: UISegmentedControl) {
+        updateImage()
+        updateCaps()
+    }
+    
+    @IBAction func updateFont(_ sender: UISwitch) {
+        updateCaps()
+    }
+    
+    @IBAction func changeFontSize(_ sender: UISlider) {
+        let fontSize = sender.value
+        fontSizeNumber.text = String(format: "%.0f", fontSize)
+        let fontSizeCGFloat=CGFloat(fontSize)
+        titleLabel.font=UIFont.systemFont(ofSize: fontSizeCGFloat)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
