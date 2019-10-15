38331eb125aa1f3fe4436eada9badede009b7d2c
Gabe <gara2591@colorado.edu>
2019-10-15 17:11:36 -0600

    update

--- Midterm/midterm/midterm/ViewController.swift
+++ Midterm/midterm/midterm/ViewController.swift
@@ -22,22 +22,9 @@ class ViewController: UIViewController, UITextFieldDelegate{
         return true
     }
     
-//    func monthlyCommute() {
-//        if showMonthlyCommute.isOn {
-//            let distance = totalDistance.value
-//            let gasLeft = numberGasMiles.value
-//            if (distance > gasLeft) {
-//                let alert=UIAlertController(title: "Warning", message: "You do not have enough gas for this commute.", preferredStyle: UIAlertController.Style.alert)
-//            }
-//            else {
-//
-//            }
-//        }
-//    }
-    
     @IBAction func changeGasMiles(_ sender: UISlider) {
         let gallonMiles = sender.value
-        numberGasMiles.text = String(format: "%.0f", gallonMiles) + String(format: " gallons")
+        numberGasMiles.text = String(format: "%.0f", gallonMiles)
     }
     
     func updateImage() {
@@ -56,21 +43,164 @@ class ViewController: UIViewController, UITextFieldDelegate{
         updateImage()
     }
     
-//    func totalCommuteTime(){
-//        //compute and update the total commute time
-//    }
-//
-//    func gas2Purchase(){
-//        //compute and update the number of gallons you need to purchase
-//    }
-//
-//    @IBAction func cummuteButton(_ sender: UIButton) {
-//        monthlyCommute()
-//        totalCommuteTime()
-//        gas2Purchase()
-//    }
+    func computeCommute() {
+        //compute and update the total commute time
+        while showMonthlyCommute.isOn {
+            if imageControl.selectedSegmentIndex == 0 {
+                var miles:Float //round trip commute miles
+                var gas:Float //gas left in tank
+                
+                if numberGasMiles.text!.isEmpty {
+                    gas = 0.0
+                }
+                else {
+                    gas = Float(numberGasMiles.text!)!
+                }
+                if totalDistance.text!.isEmpty {
+                    miles = 0.0
+                }
+                else {
+                    miles = Float(totalDistance.text!)!
+                }
+                
+                var commuteTime: Float = 0.0
+                var gallons: Float = 0.0
+                let time = (miles/20)*60
+                let price = miles/gas
+
+                if (miles > gas) {
+                    commuteTime = time
+                    gallons = price
+                }
+                else {
+                    //create a UIAlertController object
+                    let alert=UIAlertController(title: "Warning", message: "You do not have enough gas for this commute.", preferredStyle: UIAlertController.Style.alert)
+                    //create a UIAlertAction object for the button
+                    let cancelAction=UIAlertAction(title: "Cancel", style:UIAlertAction.Style.cancel, handler: nil)
+                    alert.addAction(cancelAction) //adds the alert action to the alert object
+                    let okAction=UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: {action in
+                        //self.peopleStepper.value = 1
+                        self.totalDistance.text? = "Enter Distance"
+                        //self.updateTipTotals()
+                    })
+                    alert.addAction(okAction)
+                    present(alert, animated: true, completion: nil)
+                } //end else
+
+                let currencyFormatter = NumberFormatter()
+                currencyFormatter.numberStyle=NumberFormatter.Style.currency //set the number style
+                totalCommuteTime.text=currencyFormatter.string(from: NSNumber(value: commuteTime)) //returns a formatted string
+                gas2Purchase.text=currencyFormatter.string(from: NSNumber(value: gallons))
+            }
+            
+            if imageControl.selectedSegmentIndex == 1 {
+                var miles:Float //round trip commute miles
+                var gas:Float //gas left in tank
+                
+                if numberGasMiles.text!.isEmpty {
+                    gas = 0.0
+                }
+                else {
+                    gas = Float(numberGasMiles.text!)!
+                }
+                if totalDistance.text!.isEmpty {
+                    miles = 0.0
+                }
+                else {
+                    miles = Float(totalDistance.text!)!
+                }
+                
+                var commuteTime: Float = 0.0
+                var gallons: Float = 0.0
+                
+                let time = (miles/12)*60
+                let price = miles/gas
+
+                if (miles > gas) {
+                    commuteTime = time
+                    gallons = price
+                }
+                else {
+                    //create a UIAlertController object
+                    let alert=UIAlertController(title: "Warning", message: "You do not have enough gas for this commute.", preferredStyle: UIAlertController.Style.alert)
+                    //create a UIAlertAction object for the button
+                    let cancelAction=UIAlertAction(title: "Cancel", style:UIAlertAction.Style.cancel, handler: nil)
+                    alert.addAction(cancelAction) //adds the alert action to the alert object
+                    let okAction=UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: {action in
+                        //self.peopleStepper.value = 1
+                        self.totalDistance.text? = "Enter Distance"
+                        //self.updateTipTotals()
+                    })
+                    alert.addAction(okAction)
+                    present(alert, animated: true, completion: nil)
+                } //end else
+
+                let currencyFormatter = NumberFormatter()
+                currencyFormatter.numberStyle=NumberFormatter.Style.currency //set the number style
+                totalCommuteTime.text=currencyFormatter.string(from: NSNumber(value: commuteTime)) //returns a formatted string
+                gas2Purchase.text=currencyFormatter.string(from: NSNumber(value: gallons))
+            }
+            if imageControl.selectedSegmentIndex == 2 {
+                var miles:Float //round trip commute miles
+                var gas:Float //gas left in tank
+                
+                if numberGasMiles.text!.isEmpty {
+                    gas = 0.0
+                }
+                else {
+                    gas = Float(numberGasMiles.text!)!
+                }
+                if totalDistance.text!.isEmpty {
+                    miles = 0.0
+                }
+                else {
+                    miles = Float(totalDistance.text!)!
+                }
+                
+                var commuteTime: Float = 0.0
+                var gallons: Float = 0.0
+                
+                _ = commuteTime + 10
+                _ = 0.0
+                
+                let time = (miles/10)*60
+                let price = miles/gas
+
+                if (miles > gas) {
+                    commuteTime = time
+                    gallons = price
+                }
+                else {
+                    //create a UIAlertController object
+                    let alert=UIAlertController(title: "Warning", message: "You do not have enough gas for this commute.", preferredStyle: UIAlertController.Style.alert)
+                    //create a UIAlertAction object for the button
+                    let cancelAction=UIAlertAction(title: "Cancel", style:UIAlertAction.Style.cancel, handler: nil)
+                    alert.addAction(cancelAction) //adds the alert action to the alert object
+                    let okAction=UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: {action in
+                        //self.peopleStepper.value = 1
+                        self.totalDistance.text? = "Enter Distance"
+                        //self.updateTipTotals()
+                    })
+                    alert.addAction(okAction)
+                    present(alert, animated: true, completion: nil)
+                } //end else
+
+                let currencyFormatter = NumberFormatter()
+                currencyFormatter.numberStyle=NumberFormatter.Style.currency //set the number style
+                totalCommuteTime.text=currencyFormatter.string(from: NSNumber(value: commuteTime)) //returns a formatted string
+                gas2Purchase.text=currencyFormatter.string(from: NSNumber(value: gallons))
+            }
+        }
+    }
+}
+
+
+    @IBAction func cummuteButton(_ sender: UIButton) {
+        computeCommute()
+    }
     
     override func viewDidLoad() {
+        //totalDistance.delegate=self
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
